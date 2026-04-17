628fe608f6011bb3023069c80921948500717642
Braden <fish0859@gmail.com>
2026-04-17 14:52:34 -0600

    created models and migrated

---
+++ LibraryManagement/library/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+# from library.api.views import blog_posts,blog_post
+
+urlpatterns = [
+    # path('posts/', blog_posts, name='blog-posts'),
+    # path('posts/<int:id>', blog_post, name='blog-post'),
+]
--- LibraryManagement/library/models.py
+++ LibraryManagement/library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(("name"), max_length=100)
+class Book(models.Model):
+    title = models.CharField(("title"), max_length=100)
+    author = models.CharField(("author"), max_length=100)
+    published_date = models.CharField(("published_date"), max_length=50)
+class Loan(models.Model):
+    borrower = models.CharField(("borrower"), max_length=100)
+    title = models.CharField(("title"), max_length=100)
+    loan_date = models.CharField(("loan_date"), max_length=50)
