8e68dbd3f9ffae8a939d2ed126fcab80d0acdd49
Braden <fish0859@gmail.com>
2026-04-17 17:31:56 -0600

    changed serializer to use ModelSerializer

--- LibraryManagement/library/api/serializers.py
+++ LibraryManagement/library/api/serializers.py
@@ -1,8 +1,34 @@
+from django.db.models.base import ValidationError
 from rest_framework import serializers
 from library.models import Author
 
+class AuthorSerializer(serializers.ModelSerializer):
+    name_len = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Author
+        fields = '__all__' # ['name',]
+
+    def get_name_len(self,object):
+        return (len(object.name))
+
+    def validate(self,data):
+        if len(data['name']) < 3:
+            raise serializers.ValidationError("Name is too short.")
+        else:
+            return data
+
+
+'''
+# def name_length(value):
+#     if len(value) < 3:
+#         raise serializers.ValidationError("Name is too short.")
+#     else:
+#         return value
+
 class AuthorSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(validators=[name_length])
     name = serializers.CharField()
 
     def create(self, validated_data):
@@ -12,3 +38,16 @@ class AuthorSerializer(serializers.Serializer):
         instance.name = validated_data.get('name', instance.name)
         instance.save()
         return instance
+
+    # def validate(self,data):
+        # if data['name'] == data['field2']:
+        #     raise serializers.ValidationError("should be different")
+        # else:
+        #      return data
+
+    def validate_name(self,value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name is too short.")
+        else:
+            return value
+'''
--- LibraryManagement/library/models.py
+++ LibraryManagement/library/models.py
@@ -4,12 +4,22 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(("name"), max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(("title"), max_length=100)
     author = models.CharField(("author"), max_length=100)
     published_date = models.CharField(("published_date"), max_length=50)
 
+    def __str__(self):
+        return self.title
+
 class Loan(models.Model):
     borrower = models.CharField(("borrower"), max_length=100)
     title = models.CharField(("title"), max_length=100)
     loan_date = models.CharField(("loan_date"), max_length=50)
+
+    def __str__(self):
+        return self.borrower
