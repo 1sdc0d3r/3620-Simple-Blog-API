c14c868c13bd88ae74e0520bcf17f661769284d3
Braden <fish0859@gmail.com>
2026-04-17 15:46:09 -0600

    added status codes

--- LibraryManagement/library/api/views.py
+++ LibraryManagement/library/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from library.models import Author
 from library.api.serializers import AuthorSerializer
 
@@ -15,14 +16,17 @@ def authors(req):
             cereal = AuthorSerializer(data=req.data)
             if cereal.is_valid():
                 cereal.save()
-                return Response(cereal.data)
+                return Response(cereal.data, status=status.HTTP_201_CREATED)
             else:
-                return Response(cereal.errors)
+                return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def author(req, id):
-    author = Author.objects.get(pk=id)
+    try:
+        author = Author.objects.get(pk=id)
+    except:
+        return Response({'Error': "Author not found"}, status=status.HTTP_404_NOT_FOUND)
     match req.method:
         case 'GET':
             cereal = AuthorSerializer(author)
@@ -32,9 +36,10 @@ def author(req, id):
             cereal = AuthorSerializer(author, data=req.data)
             if cereal.is_valid():
                 cereal.save()
-                return Response(cereal.data)
+                return Response(cereal.data, status=status.HTTP_200_OK)
             else:
-                return Response(cereal.errors)
+                return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
 
         case 'DELETE':
-            pass
+            author.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
