770cd5b644e1dd4a391efe8df9c8bb2b77cc4bcd
Braden <fish0859@gmail.com>
2026-04-17 18:04:49 -0600

    updated urls and added views for book and loan list/detail

--- LibraryManagement/library/api/urls.py
+++ LibraryManagement/library/api/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 # from library.api.views import authors,author
-from library.api.views import AuthorList,AuthorDetail
+from library.api.views import AuthorList,AuthorDetail,BookList,BookDetail,LoanList,LoanDetail
 
 urlpatterns = [
     path('authors/', AuthorList.as_view(), name='authors'),
     path('authors/<int:id>', AuthorDetail.as_view(), name='author'),
-    # path('books/', AuthorDetail.as_view(), name='author'),
-    # path('books/<int:id>', AuthorDetail.as_view(), name='author'),
-    # path('loans/', AuthorDetail.as_view(), name='author'),
-    # path('loans/<int:id>', AuthorDetail.as_view(), name='author'),
+    path('books/', BookList.as_view(), name='author'),
+    path('books/<int:id>', BookDetail.as_view(), name='author'),
+    path('loans/', LoanList.as_view(), name='author'),
+    path('loans/<int:id>', LoanDetail.as_view(), name='author'),
 ]
--- LibraryManagement/library/api/views.py
+++ LibraryManagement/library/api/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework import status
-from library.models import Author
-from library.api.serializers import AuthorSerializer
+from library.models import Author, Book, Loan
+from library.api.serializers import AuthorSerializer, BookSerializer, LoanSerializer
 
 class AuthorList(APIView):
     def get(self,req):
@@ -46,6 +46,94 @@ class AuthorDetail(APIView):
         except:
             return Response({'Error': "Author not found"}, status=status.HTTP_404_NOT_FOUND)
         author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BookList(APIView):
+    def get(self,req):
+        books = Book.objects.all()
+        cereal = BookSerializer(books, many=True)
+        return Response(cereal.data)
+
+    def post(self,req):
+        cereal = BookSerializer(data=req.data)
+        if cereal.is_valid():
+            cereal.save()
+            return Response(cereal.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookDetail(APIView):
+    def get(self,req,id):
+        try:
+            book = Book.objects.get(pk=id)
+        except:
+            return Response({'Error': "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        cereal = BookSerializer(book)
+        return Response(cereal.data)
+
+    def put(self,req,id):
+        try:
+            book = Book.objects.get(pk=id)
+        except:
+            return Response({'Error': "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        cereal = BookSerializer(book, data=req.data)
+        if cereal.is_valid():
+            cereal.save()
+            return Response(cereal.data, status=status.HTTP_200_OK)
+        else:
+            return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,req,id):
+        try:
+            book = Book.objects.get(pk=id)
+        except:
+            return Response({'Error': "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LoanList(APIView):
+    def get(self,req):
+        loans = Loan.objects.all()
+        cereal = LoanSerializer(loans, many=True)
+        return Response(cereal.data)
+
+    def post(self,req):
+        cereal = LoanSerializer(data=req.data)
+        if cereal.is_valid():
+            cereal.save()
+            return Response(cereal.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoanDetail(APIView):
+    def get(self,req,id):
+        try:
+            loan = Loan.objects.get(pk=id)
+        except:
+            return Response({'Error': "Loan not found"}, status=status.HTTP_404_NOT_FOUND)
+        cereal = LoanSerializer(loan)
+        return Response(cereal.data)
+
+    def put(self,req,id):
+        try:
+            loan = Loan.objects.get(pk=id)
+        except:
+            return Response({'Error': "Loan not found"}, status=status.HTTP_404_NOT_FOUND)
+        cereal = LoanSerializer(loan, data=req.data)
+        if cereal.is_valid():
+            cereal.save()
+            return Response(cereal.data, status=status.HTTP_200_OK)
+        else:
+            return Response(cereal.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,req,id):
+        try:
+            loan = Loan.objects.get(pk=id)
+        except:
+            return Response({'Error': "Loan not found"}, status=status.HTTP_404_NOT_FOUND)
+        loan.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
